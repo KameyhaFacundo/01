06cef37662ea742d3367a4917800eef1c6753056
Facundo Kameyha <kameyhafacundo@gmail.com>
2023-07-13 20:46:10 -0300

    Agregamos componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  return <div></div>;
+  return <Conteiner></Conteiner>;
 }
 
 export default App;
---
+++ src/components/Titulos.jsx
@@ -0,0 +1,9 @@
+const Titulos = () => {
+  return (
+    <section>
+      <h1>Contador con react</h1>
+    </section>
+  );
+};
+
+export default Titulos;
