9f593116811c349537d6e0235a2550a206c7ca29
Facundo Kameyha <kameyhafacundo@gmail.com>
2023-07-19 20:02:00 -0300

    agregamos una key a los componentes item tareas

--- src/components/ListaTareas.jsx
+++ src/components/ListaTareas.jsx
@@ -4,8 +4,8 @@ import ItemTarea from "./ItemTarea";
 const ListaTareas = ({ tareas }) => {
   return (
     <ListGroup>
-      {tareas.map(() => (
-        <ItemTarea> </ItemTarea>
+      {tareas.map((tarea, index) => (
+        <ItemTarea key={index}></ItemTarea>
       ))}
     </ListGroup>
   );
